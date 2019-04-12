feb36363a62cffcf16b268bcc26cfcb11e66579d
joel-b-williams <joel.williams@intercom.io>
2019-04-12 14:37:13 -0500

    migrate basic call to Intercom slack

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,43 +1,28 @@
 class WebhooksController < ApplicationController
 
   def hook
-    # p "*" * 50
-    # p params
-    # p "="* 10
-    # body = params["body"].split
-    # title = params["title"]
-    # p body
-    # p "="* 10
-    # p title
-    # p "="* 10
-    # p "Red?"
-    # p body.include?("RED")
-    # p title_contains?("Triggered")
-    # p "="* 10
-    # p "Amber?"
-    # p body.include?("AMBER")
-    # p title_contains?("Warn")
-    # p "="* 10
-    # p "Recovered?"
-    # p title_contains?("Recovered")
-    # p "*" * 50
-    
     message_payload = {
-      "token": "#{Rails.application.secrets.borker_oauth}",
-      "channel": "#borkbot"
+      "token": "#{Rails.application.secrets.borkBot_oauth}",
+      "channel": "#cse-kpi-alerts"
     }
     
     title = params["title"]
     
     if title_contains?("Warn") #amber
       message_payload["text"] = "It Ambork"
-      HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      res = HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      p "*" *50
+      p res.body
     elsif title_contains?("Triggered") #red
       message_payload["text"] = "<!here> Oh no it red!!"
-      HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      res = HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      p "*" *50
+      p res.body
     elsif title_contains?("Recovered") #resolved
       message_payload["text"] = "It all good now"
-      HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      res = HTTParty.post('https://slack.com/api/chat.postMessage', body: message_payload)
+      p "*" *50
+      p res.body
     end
   end
   
@@ -45,5 +30,4 @@ class WebhooksController < ApplicationController
     def title_contains?(word)
       params["title"].scan(word).length > 0
     end
-
 end
