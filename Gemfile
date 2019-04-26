8661bd7cfc790bd5caa9e39ce249bfc816c1ff8c
joel-b-williams <joel.williams@intercom.io>
2019-04-26 16:37:11 -0500

    api call to DD to resolve on false positive

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'puma', '~> 3.0'
 # gem 'rack-cors'
 gem 'httparty'
 gem 'intercom', '~> 3.7.6'
+gem 'pry', '~> 0.12.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,10 +8,18 @@ class WebhooksController < ApplicationController
     
     icom = Intercom::Client.new(token: Rails.application.secrets.icom_token)
     title = params["title"]
+
     slack_post = 'https://slack.com/api/chat.postMessage'
     vip_inbox = 2001586
-    kpi_limit = 120.minutes.ago.to_i
-    
+    kpi_limit = 1.minutes.ago.to_i
+
+    dd_api_key = Rails.application.secrets.dd_api_key
+    dd_app_key = Rails.application.secrets.dd_app_key
+    datadog_resolve = "https://app.datadoghq.com/monitor/bulk_resolve?api_key=#{dd_api_key}&application_key=#{dd_app_key}"
+    # datadog_resolve = "https://app.datadoghq.com/monitor/bulk_resolve?api_key=21ea07c2d43462f80d54da7180ce53f4&application_key=43f5ed7cc0cb7d2bc360e684e8d5a994234d0296"
+    cse_monitor = {"resolve": [{"8755429": "*"}]}
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+
     if title_contains?("Warn") #amber
       message_payload["text"] = "It Ambork"
       res = HTTParty.post(slack_post, body: message_payload)
@@ -19,44 +27,32 @@ class WebhooksController < ApplicationController
       convos = get_from(vip_inbox, icom) 
       full_convos = get_full_convos(convos, icom)
       assign_times = get_assignment_times(full_convos, vip_inbox)
+      red_convos = get_reds(assign_times, kpi_limit).length
       
-      if get_reds(assign_times, kpi_limit).length > 0
-        message_payload["text"] = "<!here> Oh no it red!!"
+      if red_convos > 0
+        message_payload["text"] = "<!here> Oh no it red!! #{red_convos} over KPI"
         res = HTTParty.post(slack_post, body: message_payload) 
       else
-        message_payload["text"] = "false alarm"
+        message_payload["text"] = "false alarm - Red count is #{red_convos}"
         res = HTTParty.post(slack_post, body: message_payload)
-        
-        # api_key=<YOUR_API_KEY>
-        # app_key=<YOUR_APP_KEY>
-        
-        
-        # curl -X POST -H "Content-type: application/json" \
-        # -d '{
-        # "resolve": [
-        #           {"<YOUR_MONITOR_ID>": "<YOUR_FIRST_GROUP>"},
-        #           {"<YOUR_MONITOR_ID>": "<YOUR_SECOND_GROUP>"}
-        #       ]
-        # }' \
-        #     "https://app.datadoghq.com/monitor/bulk_resolve?api_key=${api_key}&application_key=${app_key}"
-
+        resolve = HTTParty.post(datadog_resolve, :body => cse_monitor.to_json, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
       end
 
     elsif title_contains?("Recovered") #resolved
       
-      convos = get_from(vip_inbox, icom) 
-      full_convos = get_full_convos(convos, icom)
-      assign_times = get_assignment_times(full_convos, vip_inbox)
+      # convos = get_from(vip_inbox, icom) 
+      # full_convos = get_full_convos(convos, icom)
+      # assign_times = get_assignment_times(full_convos, vip_inbox)
 
       
-      if get_reds(assign_times, kpi_limit).length > 0
-        message_payload["text"] = "It all good now"
-        res = HTTParty.post(slack_post, body: message_payload) 
-      else
-        message_payload["text"] = "false alarm"
-        res = HTTParty.post(slack_post, body: message_payload)
-        #API to DD to resolve monitor
-      end
+      # if get_reds(assign_times, kpi_limit).length > 0
+      #   message_payload["text"] = "It all good now"
+      #   res = HTTParty.post(slack_post, body: message_payload) 
+      # else
+      #   message_payload["text"] = "false alarm"
+      #   res = HTTParty.post(slack_post, body: message_payload)
+      #   #API to DD to resolve monitor
+      # end
     end
   end
   
